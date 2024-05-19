25d27ea4b3933eef511b973d21596a3eeaa53a2d
John <johnmoreau19@gmail.com>
2024-05-19 16:29:10 -0700

    initial nav bar layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,75 @@
+import { useState } from "react";
+import styled from "styled-components";
 
 export default function NavBar() {
+    const [active, setActive] = useState('Home')
+
+    const handleSetActive = (navItem: string) => {
+        setActive(navItem)
+    }
+
+
     return (
-        <div>Nav Bar</div>
+        <Nav>
+            <Logo>Your Logo</Logo>
+            <NavItems>
+                {['Home', 'Portfolio', 'Blog', 'About Me', 'Contact Me'].map((navItem) => (
+                    <NavItem key={navItem}>
+                        <NavLink
+                            href="#"
+                            className={active === navItem ? 'active' : ''}
+                            onClick={() => handleSetActive(navItem)}
+                        >
+                            {navItem}
+                        </NavLink>
+                    </NavItem>
+                ))}
+            </NavItems>
+        </Nav>
     )
 }
+
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #333;
+    padding: 10px 20px;
+`;
+
+const Logo = styled.div`
+    color: #fff;
+    font-size: 1.5em;
+`;
+
+const NavItems = styled.ul`
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+`;
+
+const NavItem = styled.li`
+    margin-left: 20px;
+`;
+
+const NavLink = styled.a`
+    color: #fff;
+    text-decoration: none;
+    padding: 10px 15px;
+    display: inline-block;
+    position: relative;
+
+    &.active::after,
+    &:hover::after {
+        content: '';
+        display: block;
+        width: 100%;
+        height: 3px;
+        background-color: purple;
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+    }
+`;
